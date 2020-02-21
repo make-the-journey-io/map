fe01ada7f37ac06fe5a6ca0733d188636b36efcd
David Lantos <david.lantos@gmail.com>
2020-02-21 11:25:37 +0000

    Only show the requirements if the stage is valid
    Otherwise, it would attempt to dereference a `nil` stage.

--- main.go
+++ main.go
@@ -5,22 +5,25 @@ import (
 	"os"
 )
 
+func showLinks(links []Link) {
+	for _, link := range links {
+		fmt.Printf("   - requires: '%s' ➡ %s\n", link.stage.DisplayName, link.stage.path)
+	}
+}
+
 func main() {
 	var failed bool
 
 	for _, stage := range LoadMap().stages {
 		if len(stage.errors) == 0 {
 			fmt.Printf("✅ %s is valid\n", stage.path)
+			showLinks(stage.Requires)
 		} else {
 			failed = true
 			fmt.Printf("⛔️ %s had errors:\n", stage.path)
 			for _, err := range stage.errors {
 				fmt.Printf("   - %s\n", err)
 			}
-		}
-
-		for _, link := range stage.Requires {
-			fmt.Printf("   - requires: '%s' ➡ %s\n", link.stage.DisplayName, link.stage.path)
 		}
 	}
 
