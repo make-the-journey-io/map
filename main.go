c4798e3b932ff235ad9517c1612dd5e2f6e1783f
David Lantos <david.lantos@gmail.com>
2020-04-15 16:23:32 +0100

    Extract success and failure messaging to functions
    I find this easier to grasp.

--- main.go
+++ main.go
@@ -5,9 +5,17 @@ import (
 	"os"
 )
 
-func showLinks(links []Link) {
-	for _, link := range links {
+func showSuccess(s *Stage) {
+	fmt.Printf("✅ %s is valid\n", s.path)
+	for _, link := range s.Requires {
 		fmt.Printf("   - requires: '%s' ➡ %s\n", link.stage.DisplayName, link.stage.path)
+	}
+}
+
+func showFailure(s *Stage) {
+	fmt.Printf("⛔️ %s had errors:\n", s.path)
+	for _, err := range s.errors {
+		fmt.Printf("   - %s\n", err)
 	}
 }
 
@@ -16,14 +24,10 @@ func main() {
 
 	for _, stage := range LoadMap().stages {
 		if len(stage.errors) == 0 {
-			fmt.Printf("✅ %s is valid\n", stage.path)
-			showLinks(stage.Requires)
+			showSuccess(stage)
 		} else {
 			failed = true
-			fmt.Printf("⛔️ %s had errors:\n", stage.path)
-			for _, err := range stage.errors {
-				fmt.Printf("   - %s\n", err)
-			}
+			showFailure(stage)
 		}
 	}
 
