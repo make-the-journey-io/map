45455f22036ea202b3fe34eb7533afc5e93ac663
David Lantos <david.lantos@gmail.com>
2020-05-07 22:59:17 +0100

    Show git status for the generated map image
    My usual pattern of using the script has been
    
        bin/check-and-generate.sh
        git status  # do I need to `git add`?
    
    So now `git status` is part of the script.

--- bin/check-and-generate.sh
+++ bin/check-and-generate.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -e
+target="$(git rev-parse --show-toplevel)/docs/map.png"
 go run .
-go run . -graph | dot -Gdpi=200 -Tpng -o"$(git rev-parse --show-toplevel)/docs/map.png"
+go run . -graph | dot -Gdpi=200 -Tpng -o"$target"
+git status --short "$target"
